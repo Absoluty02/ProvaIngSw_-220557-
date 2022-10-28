27af02814f20091dfa0591b88db696e12f04761b
Jacopo Garofalo <jacopogarofalo02@gmail.com>
2022-10-28 17:32:20 +0200

    Fifth commit: added joda-time and last test case

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -9,6 +9,11 @@ public class App
     public static void main( String[] args )
     {
         FunnyAlgorithms f = new FunnyAlgorithms();
-        System.out.println(f.stringToIntConverter("400000"));
+
+        int[] array = new int[]{5,23,10,43,67};
+        f.selectionSort(array, 1);
+        for(int i = 0; i < array.length; i++){
+            System.out.println(array[i]);
+        }
     }
 }
--- src/test/java/org/example/FunnyAlgorithmsTest.java
+++ src/test/java/org/example/FunnyAlgorithmsTest.java
@@ -5,6 +5,7 @@ import org.junit.*;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -20,35 +21,66 @@ public class FunnyAlgorithmsTest
     int binarySearchTarget;
     int binarySearchTargetPosition;
     int[] binarySearchArray;
-    int[] UnorderedArray;
-    int[] OrderedArray;
+    int[] UnorderedArray1;
+    int[] UnorderedArray2;
+    int[] IncreasingOrderedArray;
+    int[] DecreasingOrderedArray;
     int convertedNumber;
 
     FunnyAlgorithms f;
 
     @Before
     public void ArraysInitialize(){
+
+        System.out.println("Initialization starts at " + LocalDateTime.now());
+
         binarySearchArray = new int[]{1,2,3,4,5,6,7};
         binarySearchTarget = 4;
         binarySearchTargetPosition = 3;
 
         f = new FunnyAlgorithms();
 
-        UnorderedArray = new int[]{10,43,23,5,67};
-        OrderedArray = new int[]{5,10,23,43,67};
+        UnorderedArray1 = new int[]{10,43,23,5,67};
+        UnorderedArray2 = new int[]{10,43,23,5,67};
+        IncreasingOrderedArray = new int[]{5,10,23,43,67};
+        DecreasingOrderedArray = new int[]{67,43,23,10,5};
 
         convertedNumber = 43;
+
+        System.out.println("Initialization finished at " + LocalDateTime.now());
+
+
+    }
+
+    @Test
+    public void SelectionSortTest(){
+
+        System.out.println("Test starts at " + LocalDateTime.now());
+
+        f.selectionSort(UnorderedArray1, 0);
+        Assert.assertArrayEquals(IncreasingOrderedArray, UnorderedArray1);
+
+        System.out.println("Test finished at " + LocalDateTime.now());
 
     }
 
     @Test
     public void BinarySearchTest(){
+
+        System.out.println("Test starts at " + LocalDateTime.now());
+
         Assert.assertEquals(binarySearchTargetPosition, f.binarySearch(binarySearchArray, binarySearchTarget));
+
+        System.out.println("Test finished at " + LocalDateTime.now());
     }
 
     @Test
     public void StringToIntConverterTest()
     {
+        System.out.println("Test starts at " + LocalDateTime.now());
+
         Assert.assertEquals( convertedNumber, f.stringToIntConverter("43") );
+
+        System.out.println("Test finished at " + LocalDateTime.now());
     }
 }
